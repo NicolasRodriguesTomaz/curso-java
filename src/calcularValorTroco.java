14a689c30e5be3c699c68f693f05222332e2cf89
NicolasRodriguesTomaz <nicolasrodriguestomaz@gmail.com>
2025-06-23 04:47:40 -0300

    teste

---
+++ src/calcularValorTroco.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+
+public class CalcularValorTroco { 
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("digite o valor do produto: ");
+        Double valorProduto = scanner.nextDouble();
+
+        System.out.print("digite a quantidade passada pelo cliente: ");
+        Double valorPassadoPeloCliente = scanner.nextDouble();
+
+        Double resultado = valorPassadoPeloCliente - valorProduto;
+
+        System.out.println("troco: " + resultado);
+
+        scanner.close(); 
+    }
+    
+}
