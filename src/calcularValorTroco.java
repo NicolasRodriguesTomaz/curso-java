2ca48eee87ecb3bc451dc59769931d9ed7686c05
NicolasRodriguesTomaz <nicolasrodriguestomaz@gmail.com>
2025-06-23 11:15:31 -0300

    correçao

--- src/calcularValorTroco.java
+++ src/calcularValorTroco.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; 
 
-public class CalcularValorTroco { 
+public class calcularValorTroco { 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
