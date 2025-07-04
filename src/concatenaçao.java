3ae6708ffa4c45154da12b96a65f01875293b938
NicolasRodriguesTomaz <nicolasrodriguestomaz@gmail.com>
2025-07-04 10:34:33 -0300

    concatenação, incremento e decremento

---
+++ src/concatenaçao.java
@@ -0,0 +1,10 @@
+public class concatenaçao {
+    public static void main(String[] args) {
+        String inicio = "esse ";
+        String meio = " e o ";
+        String fim = " exemplo";
+        Integer numeroExemplo = 2;
+        System.out.println(inicio +numeroExemplo + meio + fim);
+        System.out.println("esse" + meio + "exemplo");
+    }
+}
---
+++ src/operadoresIncrementoDecremento.java
@@ -0,0 +1,18 @@
+public class operadoresIncrementoDecremento {
+    public static void main(String[] args) {
+//        Integer numero = 10;
+  //       System.out.println("numero antes:" + numero);
+    //   numero++;
+      //  System.out.println("numero depois:" + numero);
+
+
+        //Integer numero = 10;
+       // Integer numero2 = ++numero;
+        //Integer numero2 = numero++;
+        //System.out.println("numero " + numero + "," + "numero 2 " + numero2);
+
+        Integer numero = 10;
+        --numero;
+        System.out.println("numero: " + numero);
+    }
+}
